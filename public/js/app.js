304aa18aec8ab138ac26da295bc907467dfb1584
Devang Mistry <devang.mistry@somaiya.edu>
2021-04-10 11:26:45 +0530

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,24 @@
-const weatherForm = document.querySelector('form');
+const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
-const messegeOne = document.querySelector('#message-1');
+const messegeOne = document.querySelector("#message-1");
 const messegeTwo = document.querySelector("#message-2");
 
-
-weatherForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    messegeOne.textContent = 'Loading ... ';
+weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    messegeOne.textContent = "Loading ... ";
     messegeTwo.textContent = "";
     const location = search.value;
-   fetch(`http://localhost:3000/weather?address=${location}`).then(
-       (response) => {
-           response.json().then((data) => {
-               if (data.error) {
-                   messegeOne.textContent = data.error;
-               } else {
-                   messegeOne.textContent = data.location;
-                   messegeTwo.textContent = data.forecastData;
-               }
-           });
-       }
-   );
-})
+
+    fetch(`/weather?address=${location}`).then(
+        (response) => {
+            response.json().then((data) => {
+                if (data.error) {
+                    messegeOne.textContent = data.error;
+                } else {
+                    messegeOne.textContent = data.location;
+                    messegeTwo.textContent = data.forecastData;
+                }
+            });
+        }
+    );
+});
